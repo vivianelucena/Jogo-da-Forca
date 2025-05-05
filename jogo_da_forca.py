9dceceb0cb1365c8026a94810fed634ac5db8e1e
Viviane <vivianedelucenasilva@gmail.com>
2025-05-05 00:03:03 -0300

    commit

---
+++ jogo_da_forca.py
@@ -0,0 +1,38 @@
+palavra = 'paralelepipedo'
+letraCerta = ['_'] * len(palavra)
+chances = 5
+
+print("Jogo da Forca")
+print("Palavra:", " ".join(letraCerta))
+
+while True:
+    letra = input(f'Digite uma letra: ').lower()
+
+    if len(letra) != 1:
+        print(f'Digite apenas uma letra: ')
+        continue
+
+
+    if letra in palavra:
+        for a in range(len(palavra)):
+            if palavra[a] == letra:
+               letraCerta[a] = letra
+        print(f'Acertou!')
+        print("Palavra:", " ".join(letraCerta))
+    else:
+        chances = chances - 1
+        print(f'Errou!\n Tentativas restantes: {chances}')
+        print("Palavra:", " ".join(letraCerta))
+
+
+    if chances == 0:
+        print(f'Você perdeu!')
+        break
+
+    if '_' not in letraCerta:
+        break
+
+if '_' not in letraCerta:
+    print('Parabéns! Você ganhou o jogo!')
+else:
+    print(f'A palavra era: {palavra}')
